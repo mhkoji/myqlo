96fe42dc79ae928b7204c6471fee2c409b07b182
mhkoji <mhkoji2+git@gmail.com>
2021-05-03 00:25:23 +0900

    Update test

--- t/src/query-and-execute.lisp
+++ t/src/query-and-execute.lisp
@@ -14,11 +14,11 @@
     )")
   (myqlo:query conn
    "INSERT INTO users (name, created_on)
-      VALUES (\"AAA\", \"2021-01-01 10:00:00\")")
+      VALUES (\"A123\", \"2021-01-01 10:00:00\")")
   (myqlo:execute conn
    "INSERT INTO users (name, created_on) VALUES (?, ?)"
    (list (myqlo:make-param
-          :sql-type :string :value "BBB")
+          :sql-type :string :value "BB456789")
          (myqlo:make-param
           :sql-type :string :value "2021-01-01 12:00:00"))))
 
@@ -28,23 +28,23 @@
     (is (equal
          (myqlo:query conn
           "SELECT * from users")
-         '((1 "AAA" "2021-01-01 10:00:00")
-           (2 "BBB" "2021-01-01 12:00:00"))))
+         '((1 "A123" "2021-01-01 10:00:00")
+           (2 "BB456789" "2021-01-01 12:00:00"))))
     (is (equal
          (myqlo:execute conn
           "SELECT * from users WHERE user_id = ?"
           (list (myqlo:make-param
                  :sql-type :long :value 2)))
-         '((2 "BBB" "2021-01-01 12:00:00"))))
+         '((2 "BB456789" "2021-01-01 12:00:00"))))
     (is (equal
          (myqlo:execute conn
           "SELECT * from users WHERE name = ?"
           (list (myqlo:make-param
-                 :sql-type :string :value "AAA")))
-        '((1 "AAA" "2021-01-01 10:00:00"))))
+                 :sql-type :string :value "A123")))
+        '((1 "A123" "2021-01-01 10:00:00"))))
     (is (equal
          (myqlo:execute conn
           "SELECT * from users WHERE created_on < ?"
           (list (myqlo:make-param
                  :sql-type :string :value "2021-01-1 11:00:00")))
-         '((1 "AAA" "2021-01-01 10:00:00"))))))
+         '((1 "A123" "2021-01-01 10:00:00"))))))
